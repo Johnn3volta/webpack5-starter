d8e14a0e96a5dbc2108854688c3d43e7d42341dd
Mikhail <johnn3volta@gmail.com>
2021-04-29 17:41:11 +0300

    Update paths.js
    add slash to fonts path

--- configs/paths.js
+++ configs/paths.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const paths = {
   src: path.resolve(__dirname, '../src'),
-  fonts: path.resolve(__dirname, '../src/fonts'),
+  fonts: path.resolve(__dirname, '../src/fonts/'),
   images: path.resolve(__dirname, '../src/images'),
   scss: path.resolve(__dirname, '../src/styles/scss'),
   js: path.resolve(__dirname, '../src/js'),
